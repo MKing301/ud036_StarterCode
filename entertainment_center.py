289b1172c83fc32e5d59a1a2522013a2c1235567
MKING301 <mking301@gmail.com>
2018-04-06 12:56:48 -0400

    added add'l comments to entertainment_center.py file

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,19 @@
 import fresh_tomatoes
 import media
+'''
+fresh_tomatoes.py is a Udacity provided Python module
+that contains a function called open_movies_page
+that takes in one argument, which is a list of movies
+and creates an HTML file which visualize the list
+of movies
 
+media is a python module that contains the movie class, which the constructor
+for the movie class is written
+
+'''
+
+# courageous is an instance of the Movie class in the Media module for the
+# movie Courageous
 courageous = media.Movie("Courageous",
                          '''When a tragedy strikes close to home, four police
                          officers struggle with their faith and their roles as
@@ -11,6 +24,8 @@ courageous = media.Movie("Courageous",
                          "https://www.youtube.com/watch?v=rLYmku9OJ6A",
                          "Released - Aug 26, 2011")
 
+# do_you_believe is an instance of the Movie class in the Media module for the
+# movie Do You Believe
 do_you_believe = media.Movie("Do You Believe?",
                              '''When a pastor is shaken by the visible faith of
                              a street corner preacher, he is reminded that true
@@ -23,6 +38,8 @@ do_you_believe = media.Movie("Do You Believe?",
                              "https://www.youtube.com/watch?v=9kBcmuGuS7M",
                              "Released - Mar 20, 2015")
 
+# fireproof is an instance of the Movie class in the Media module for the
+# movie Firerproof
 fireproof = media.Movie("Fireproof",
                         ''' In an attempt to save his marriage, a firefighter
                         uses a 40-day experiment known as "The Love Dare".''',
@@ -31,6 +48,8 @@ fireproof = media.Movie("Fireproof",
                         "https://www.youtube.com/watch?v=YK5-5qf9IQs",
                         "Released - Sep 26, 2008")
 
+# gods_not_dead is an instance of the Movie class in the Media module for the
+# movie God's Not Dead
 gods_not_dead = media.Movie("God's Not Dead",
                             '''College philosophy professor Mr. Radisson's
                             curriculum is challenged by his new student, Josh,
@@ -40,6 +59,8 @@ gods_not_dead = media.Movie("God's Not Dead",
                             "https://www.youtube.com/watch?v=bMjo5f9eiX8",
                             "Released - Apr 11, 2014")
 
+# risen is an instance of the Movie class in the Media module for the
+# movie Risen
 risen = media.Movie("Risen",
                     '''Witness one of the most important events in human
                     history through the eyes of a nonbeliever.
@@ -50,6 +71,8 @@ risen = media.Movie("Risen",
                     "https://www.youtube.com/watch?v=OcTVLfn5i8g",
                     "Released - Feb 19, 2016")
 
+# war_room is an instance of the Movie class in the Media module for the
+# movie War Room
 war_room = media.Movie("War Room",
                        '''A seemingly perfect family looks to fix their
                        problems with the help of Miss Clara, an older,
@@ -60,14 +83,18 @@ war_room = media.Movie("War Room",
                        "https://www.youtube.com/watch?v=2DbRwcrhiLA",
                        "Released - Nov 27, 2015")
 
-# List of my favorite movies; each an instance of the Movie class
-movies = [courageous, do_you_believe, fireproof, gods_not_dead,
-          risen, war_room]
+# movies is a list of my favorite movies; each an instance of the Movie class
+movies = [courageous, do_you_believe, fireproof, gods_not_dead, risen,
+          war_room]
 
 
 fresh_tomatoes.open_movies_page(movies)
-'''fresh_tomatoes.py is a Udacity provided Python module
-that contains a function called open_movies_page
-that takes in one argument, which is a list of movies
-and creates an HTML file which visualize the list
-of movies '''
+'''
+open_movies_page function contained in the fresh_tomatoes module takes in one
+argument, which is a list of movies and creates an HTML file which visualize
+the list of movies
+
+Input(s):
+movies - list of movies created for the library
+
+'''
